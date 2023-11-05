9c1288d51e593f650ec1c24390ace489271213b3
Tarun Kumar Vadthya <142934879+Hehe-Hah@users.noreply.github.com>
2023-11-05 07:18:00 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,14 @@
+function skillsMember() {
+  var member = document.getElementById("member");
+  var skills = document.getElementById("skills");
+  var memberText = document.getElementById("memberText");
+  var skillsText = document.getElementById("skillsText");
+  var memberBorder = document.getElementById("memberBorder");
+  var skillsBorder = document.getElementById("skillsBorder");
+  member.style.color = "#000000";
+  skills.style.color = "#ffffff";
+  memberText.style.display = "block";
+  skillsText.style.display = "none";
+  memberBorder.style.display = "block";
+  skillsBorder.style.display = "none";
+}
